ee48516d3f754bea02a227f8c235a134a3a0e77e
kmpfwgn <ilya.gayduk.99@mail.ru>
2018-06-02 20:17:38 +0300

    Add database connection

--- src/main/java/by/bntu/fitr/povt/whitebearteam/config/SpringConfig.java
+++ src/main/java/by/bntu/fitr/povt/whitebearteam/config/SpringConfig.java
@@ -1,7 +1,27 @@
 package by.bntu.fitr.povt.whitebearteam.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/shop?useSll=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("122333");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
 }
