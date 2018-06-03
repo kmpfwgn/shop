3bf2e58191fb85942da2478d155e19937e906b8e
kmpfwgn <ilya.gayduk.99@mail.ru>
2018-06-03 13:50:04 +0300

    Add product rest controller, configure Json converter

--- src/main/java/by/bntu/fitr/povt/whitebearteam/config/WebConfig.java
+++ src/main/java/by/bntu/fitr/povt/whitebearteam/config/WebConfig.java
@@ -3,11 +3,15 @@ package by.bntu.fitr.povt.whitebearteam.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -27,5 +31,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(final ResourceHandlerRegistry registry){
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
         registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        converters.add(new MappingJackson2HttpMessageConverter());
+        super.configureMessageConverters(converters);
     }
 }
---
+++ src/main/java/by/bntu/fitr/povt/whitebearteam/controller/ProductController.java
@@ -0,0 +1,41 @@
+package by.bntu.fitr.povt.whitebearteam.controller;
+
+import by.bntu.fitr.povt.whitebearteam.entity.Product;
+import by.bntu.fitr.povt.whitebearteam.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    public ProductService productService;
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Product> getAllProducts(){
+        return productService.findAll();
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public void saveProduct(@RequestBody Product product){
+        productService.save(product);
+    }
+
+    @RequestMapping(value = "/{articul}", method = RequestMethod.GET)
+    public Product getProductByArticul(@PathVariable("articul") String articul){
+        return productService.getByArticul(articul);
+    }
+
+    @RequestMapping(value = "/delete/{articul}", method = RequestMethod.POST)
+    public void deleteProduct(@PathVariable("articul") String articul){
+        productService.delete(articul);
+    }
+
+    @RequestMapping(value = "/update/{articul}", method = RequestMethod.POST)
+    public void updateProduct(@RequestBody Product product){
+        productService.update(product);
+    }
+}
